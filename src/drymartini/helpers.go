29d9c63548df57ddebfba8770403855c1867ae43
Marcel Flores <marcel@fourinhand.cs.northwestern.edu>
2013-03-11 17:47:17 -0500

    helpers

---
+++ src/drymartini/helpers.go
@@ -0,0 +1,31 @@
+package drymartini
+
+import (
+    "crypto/rsa"
+    "math/big"
+    "net"
+    "log"
+)
+
+type MartiniContactReady struct {
+    PubKey rsa.PublicKey
+    NodeIP net.IP
+    NodePort uint16
+}
+
+func (mc *MartiniContact) GetReadyContact() *MartiniContactReady {
+    var mcr *MartiniContactReady
+
+    mcr = new(MartiniContactReady)
+    mcr.PubKey.N = new(big.Int)
+
+    mcr.PubKey.N.SetString(mc.PubKey, 0)
+    mcr.PubKey.E = mc.PubExp
+    mcr.NodeIP = net.ParseIP(mc.NodeIP)
+    mcr.NodePort = mc.NodePort
+
+    return mcr
+}
+
+
+
